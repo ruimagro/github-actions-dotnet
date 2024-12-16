de7d6e2b7b8e99330c33c13c81b35ee8b91c68b8
ruimagro <rmagro@gmail.com>
2024-12-16 14:27:14 +0100

    add blank space (#1)
    * add blank space
    
    * fix to yml
    
    * trying new version of dotnet
    
    * wow
    
    * still trying
    
    * pew-pew
    
    * bit of a wipe
    
    * remove test file
    
    * update to v9
    
    * simple commit for unit test
    
    * fixed, added back in a valid unit test
    
    * break

---
+++ WeatherApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WeatherApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
