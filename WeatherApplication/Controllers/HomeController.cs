e581df3cec6debd856133c3e3db945a5a8dda54d
Rui Magro <rui.developer@icloud.com>
2024-12-16 14:55:26 +0100

    editor config linting and format fixes

--- WeatherApplication/Controllers/HomeController.cs
+++ WeatherApplication/Controllers/HomeController.cs
@@ -1,31 +1,16 @@
+namespace WeatherApplication.Controllers;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using WeatherApplication.Models;
 
-namespace WeatherApplication.Controllers;
-
-public class HomeController : Controller
+public class HomeController(ILogger<HomeController> logger) : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly ILogger<HomeController> _logger = logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+    public IActionResult Index() => View();
 
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+    public IActionResult Privacy() => View();
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+    public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 }
