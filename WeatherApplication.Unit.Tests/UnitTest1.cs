08d373bd63587ee1daebc013fe68312f9aa8206a
Rui Magro <rui.developer@icloud.com>
2024-12-16 14:02:55 +0100

    simple commit for unit test

---
+++ WeatherApplication.Unit.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace WeatherApplication.Unit.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
